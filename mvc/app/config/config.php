469107013bbf36bce37223cfe8a21813adf75e9d
caprio <203040036@mail.ac.id>
2022-10-10 21:20:18 +0700

    menyelesaikan phpmvc

---
+++ mvc/app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL', 'http://localhost/prak_pw_203040036_A/mvc/public');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc');
---
+++ mvc/app/views/home/index.php
@@ -0,0 +1,10 @@
+<div class="container">
+  <div class="jumbotron mt-4">
+  <img src="<?= BASEURL; ?>/img/gok.jpg" alt="Caprio Parlindungan Sinaga" width="350" height="300" class="rounded-circle shadow">
+    <h1 class="display-4">Selamat Datang di Website Saya!</h1>
+    <p class="lead">Semoga kamu bahagia selalu </p>
+    <hr class="my-4">
+    
+    <a class="btn btn-primary btn-lg" href="" role="button">Jangan Lupa Bahagia!</a>
+  </div>
+</div>
